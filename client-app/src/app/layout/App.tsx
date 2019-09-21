93ae594ea468808dd340a47b8808cfced2be7dab
Ricky <rcruz78@outlook.com>
2019-09-21 07:58:07 -0400

    Environment set in React app and some styling

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, Fragment } from "react";
 import axios from "axios";
-import { Header, Icon, List, Container } from "semantic-ui-react";
+import { Container } from "semantic-ui-react";
 import { IActivity } from "./../models/activity";
 import NavBar from "../../features/nav/NavBar";
 import ActivityDashboard from "./../../features/activities/dashboard/ActivityDashboard";
