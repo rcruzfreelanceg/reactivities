4db8f9b6f9e21928ae9cd0302566155bcae7dcd3
Ricardo Cruz <rcruz@inmediata.com>
2019-10-02 12:11:18 -0400

    Migrating to Core 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -22,6 +22,7 @@ namespace API
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        [System.Obsolete]
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(opt => 
@@ -45,6 +46,7 @@ namespace API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        [System.Obsolete]
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             app.UseMiddleware<ErrorHandlingMiddleware>();
